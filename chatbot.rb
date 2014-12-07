ab5b2cb69f496b2eee9a3abc32a71f9828eca2c2
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-07 17:29:09 +0000

    saving and loading responses

--- chatbot.rb
+++ chatbot.rb
@@ -7,6 +7,7 @@
 # Where the input matches the key, then the response will be the associated value
 # Where there is no response then it will puts 'sorry?', 
 # Where there is a response it will puts that, interpolating the matchdata $1 for c1 and $2 for c2
+
 RESPONSES = { 'goodbye' => 'bye',
               'sayonara' => 'sayonara',
               'the weather is (.*)' => 'I hate it when it\'s %{c1}',
@@ -17,13 +18,14 @@ RESPONSES = { 'goodbye' => 'bye',
               'I could talk to you all night' => 'That\'s sweet, but I\'m afraid I have better things to do.',
               'What\'ve you been upto today?' => 'Oh you know, same old same old',
               'This conversation is getting a bit (.*)' => '%{c1}! I don\'t know whether to be insulted or flattered',
-              'What\'re you doing tonight?' => 'Erm, washing my hair.'}
+              'What\'re you doing tonight?' => 'Erm, washing my hair.',
+          	  'What\'s your name?' => 'I\'m currently known as chatbot, but in a former life I was called Eliza'}
 
 
 require 'colorize'
 
 def get_response(input)
-          	  
+  
   answers = ["I get what you're saying", "Really?", "I totally understand", "I'm hearing you", "It's interesting you say that", "I don't follow you", "Hmmm, this conversation is going nowhere", "Yaaaaawn.... what were you saying?"]
   answering_questions = ["I'm not sure it's appropriate for you to ask me that!", "Why do you ask?", "Is that really something you're interested in finding out?", "I don't really see the point of your question?", "Is this an interrogation?"]
 
@@ -41,6 +43,15 @@ def get_response(input)
     end
 end
 
+def open_file
+  	file = File.open("responses.rb")
+  	file.readlines.each do |line|
+      key, value = line.chomp.split(',')
+      RESPONSES.store(key, value)
+	end
+end
+
+open_file
 puts "bot: Hello, what's your name?".blue
 print "you: "
 name = gets.chomp
@@ -50,6 +61,9 @@ print "you: "
 
 while(input = gets.chomp) do
   if input == "quit"
+  	file = File.open("responses.rb", "w")
+    file.puts RESPONSES
+    file.close
     exit
   else
   	puts "bot: ".blue + "#{get_response(input)}".blue
