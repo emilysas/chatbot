155b3830c61131fc6c18ad5fed0ecbf28205e2a9
Sanda Golcea <sanda.golcea@gmail.com>
2014-12-07 00:33:51 +0000

    Fixes adding responses from chat

--- chatbot.rb
+++ chatbot.rb
@@ -7,30 +7,30 @@
 # Where the input matches the key, then the response will be the associated value
 # Where there is no response then it will puts 'sorry?', 
 # Where there is a response it will puts that, interpolating the matchdata $1 for c1 and $2 for c2
+RESPONSES = { 'goodbye' => 'bye',
+              'sayonara' => 'sayonara',
+              'the weather is (.*)' => 'I hate it when it\'s %{c1}',
+              'I love (.*)' => 'I love %{c1} too',
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
+              'I\'m hungry for (.*)' => 'I can\'t have %{c1}, on account of my allergies',
+              'I can\'t stand (.*)!' => 'Interesting! What is it about %{c1} that you don\'t like?',
+              'I could talk to you all night' => 'That\'s sweet, but I\'m afraid I have better things to do.',
+              'What\'ve you been upto today?' => 'Oh you know, same old same old',
+              'This conversation is getting a bit (.*)' => '%{c1}! I don\'t know whether to be insulted or flattered',
+              'What\'re you doing tonight?' => 'Erm, washing my hair.'}
 
 
 require 'colorize'
 
 def get_response(input)
-  responses = { 'goodbye' => 'bye',
-              'sayonara' => 'sayonara',
-              'the weather is (.*)' => 'I hate it when it\'s %{c1}',
-              'I love (.*)' => 'I love %{c1} too',
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
-          	  'I\'m hungry for (.*)' => 'I can\'t have %{c1}, on account of my allergies',
-          	  'I can\'t stand (.*)!' => 'Interesting! What is it about %{c1} that you don\'t like?',
-          	  'I could talk to you all night' => 'That\'s sweet, but I\'m afraid I have better things to do.',
-          	  'What\'ve you been upto today?' => 'Oh you know, same old same old',
-          	  'This conversation is getting a bit (.*)' => '%{c1}! I don\'t know whether to be insulted or flattered',
-          	  'What\'re you doing tonight?' => 'Erm, washing my hair.'}
           	  
   answers = ["I get what you're saying", "Really?", "I totally understand", "I'm hearing you", "It's interesting you say that", "I don't follow you"]
 
-  key = responses.keys.select {|k| /#{k}/ =~ input }.sample 
+  key = RESPONSES.keys.select {|k| /#{k}/ =~ input }.sample 
   /#{key}/ =~ input
-  response = responses[key]
+  response = RESPONSES[key]
     if response.nil? 
-      responses.store(input, answers.sample)
+      RESPONSES.store(input, answers.sample)
     else 
       response % { c1: $1, c2: $2, c3: $3, c4: $4, c5: $5}
     end
@@ -47,7 +47,7 @@ while(input = gets.chomp) do
   if input == "quit"
     exit
   else
-  	puts "bot: ".blue+get_response(input).blue
+  	puts "bot: ".blue + "#{get_response(input)}".blue
   	print "you: "
   end
 end
